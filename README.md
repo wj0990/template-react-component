4029ea6eebe5507aee4da498ae474f7dc12b1db8
wangjie <wj@nihaosi.com>
2021-06-14 16:18:29 +0800

    feat: 添加实例预览。

--- README.md
+++ README.md
@@ -1,14 +1,34 @@
 React Component Example for TypeScript.
 ===
+<!--rehype:style=color:red;-->
 
 Create a project for the React component library containing a website preview of the component library instance. The documents and component libraries are put into a project, all written in `TypeScript`, the component library source files are added to the `src` directory, and the document website source files are added to the `website` directory.
 
-## Quick Start
+## Install
 
 ```bash
-$ npx create-kkt my-app -e react-component-tsx
-cd my-app
-npm install
+npm i @wangchuxi/template
+```
+
+## Basic Usage
+
+
+<!--rehype:codeSandbox=true&codePen=true-->
+```jsx
+import ReactDOM from 'react-dom';
+import Button from '@wangchuxi/template';
+
+const Demo = () => {
+  return (
+    <div>
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="warning">Warning</Button>
+      <Button type="danger">Danger</Button>
+    </div>
+  )
+};
+ReactDOM.render(<Demo />, _mount_);
 ```
 
 **development**
--- website/App.tsx
+++ website/App.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import MarkdownPreview from '@uiw/react-markdown-preview';
 import GitHubCorners from '@uiw/react-github-corners';
+import rehypeAttr from 'rehype-attr';
+import Code, { CodeProps } from './Code';
 import logo from './logo.svg';
 import './App.css';
 import Button from '../';
 import MDStr from '../README.md';
+
+const getCodeStr = (data: any[] = [], code: string = '') => {
+  data.forEach((node) => {
+    if (node.type === 'text') {
+      code += node.value;
+    } else if (node.children && Array.isArray(node.children)) {
+      code += getCodeStr(node.children);
+    }
+  });
+  return code;
+};
 
 const App: React.FC = () => {
   return (
@@ -27,7 +40,34 @@ const App: React.FC = () => {
           <Button type="dark">Dark</Button>
         </div>
       </header>
-      <MarkdownPreview source={MDStr} className="info" />
+      <MarkdownPreview
+        source={MDStr}
+        className="info"
+        rehypePlugins={[[rehypeAttr, { properties: 'attr' }]]}
+        components={{
+          /**
+           * bgWhite 设置代码预览背景白色，否则为格子背景。
+           * noCode 不显示代码编辑器。
+           * noPreview 不显示代码预览效果。
+           * noScroll 预览区域不显示滚动条。
+           * codePen 显示 Codepen 按钮，要特别注意 包导入的问题，实例中的 import 主要用于 Codepen 使用。
+           */
+          code: ({ inline, node, noPreview, noScroll, bgWhite, noCode, codePen, codeSandbox, ...props }) => {
+            const conf = { noPreview, noScroll, bgWhite, noCode, codePen, codeSandbox } as CodeProps;
+            if (noPreview || noScroll || bgWhite || noCode || codePen || codeSandbox) {
+              return (
+                <Code
+                  {...conf}
+                  code={getCodeStr(node.children)}
+                  dependencies={{ useRef, useEffect, useState, Button }}
+                  language={(props.className || '').replace(/^language-/, '')}
+                />
+              );
+            }
+            return <code {...props} />;
+          },
+        }}
+      />
     </div>
   );
 };
