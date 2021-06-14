bf97f7864a148d07fd84856b8791bf8b171057c5
wangjie <wj@nihaosi.com>
2021-06-14 16:05:32 +0800

    init porject.

---
+++ src/index.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import styles from './style/index.module.less';
+import './style/index.less';
+
+export interface ButtonProps {
+  prefixCls?: string;
+  type?: string;
+  size?: 'large' | 'default' | 'small';
+  active?: boolean;
+  disabled?: boolean;
+  block?: boolean;
+  basic?: boolean;
+  loading?: boolean;
+  className?: string;
+  children?: React.ReactNode;
+  htmlType?: React.ButtonHTMLAttributes<HTMLButtonElement>['type'];
+}
+export default function Button(props: ButtonProps = {}) {
+  const {
+    prefixCls = 'w-btn',
+    disabled = false,
+    active = false,
+    loading = false,
+    block = false,
+    basic = false,
+    htmlType = 'button',
+    type = 'light',
+    size = 'default',
+    className,
+    children,
+    ...others
+  } = props;
+
+  const cls = [
+    className,
+    prefixCls,
+    styles.test,
+    size ? `${prefixCls}-${size}` : false,
+    type ? `${prefixCls}-${type}` : false,
+    basic ? `${prefixCls}-basic` : false,
+    loading ? `${prefixCls}-loading` : false,
+    disabled || loading ? 'disabled' : false,
+    active ? 'active' : false,
+    block ? 'block' : false,
+  ]
+    .filter(Boolean)
+    .join(' ');
+  return (
+    <button {...others} disabled={disabled || loading} type={htmlType} className={cls}>
+      {children &&
+        React.Children.map(children, (child) => {
+          if (React.isValidElement(child)) return child;
+          return <span> {child} </span>;
+        })}
+    </button>
+  );
+}
---
+++ website/App.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import MarkdownPreview from '@uiw/react-markdown-preview';
+import GitHubCorners from '@uiw/react-github-corners';
+import logo from './logo.svg';
+import './App.css';
+import Button from '../';
+import MDStr from '../README.md';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <GitHubCorners fixed href="https://github.com/kktjs/kkt" />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        <div>
+          <Button type="primary">Primary</Button>
+          <Button type="success">Success</Button>
+          <Button type="warning">Warning</Button>
+          <Button type="danger">Danger</Button>
+          <Button type="light">Light</Button>
+          <Button type="dark">Dark</Button>
+        </div>
+      </header>
+      <MarkdownPreview source={MDStr} className="info" />
+    </div>
+  );
+};
+
+export default App;
